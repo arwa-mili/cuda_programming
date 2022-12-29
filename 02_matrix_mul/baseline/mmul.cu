062b371acdb74aea3ce05dd13c32dc5b849f38d1
rw@@arwa <64184160+arwa-mili@users.noreply.github.com>
2022-12-29 14:19:41 +0100

    Update mmul.cu

--- 02_matrix_mul/baseline/mmul.cu
+++ 02_matrix_mul/baseline/mmul.cu
@@ -46,7 +46,7 @@ void verify_result(vector<int> &a, vector<int> &b, vector<int> &c, int N) {
 
 int main() {
   // Matrix size of 1024 x 1024;
-  int N = 1 << 10;
+  int N = 1 << 10;  // == 1*(2^10) = 1024 
 
   // Size (in bytes) of matrix
   size_t bytes = N * N * sizeof(int);
